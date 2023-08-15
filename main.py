a3b3e67cea67b613ff7db4e83ccfbd566800c1cf
wugluskr1 <54072591+wugluskr1@users.noreply.github.com>
2023-08-15 17:29:11 +0300

    добавил вычисление факториала

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 #2. Реализуйте рекурсивную функцию для подсчета элементов в списке.
 #3. Напишите функцию, которая при каждом следующем своем вызове выводит на консоль следующий элемент заданного списка.
 from random import randint
-numbers =  [randint(1, 100) for i in range(20)]
-def filter_odd_num(in_num):
-    if(in_num % 2) != 0:
+
+j = int(input("Введите число символов в массиве: "))
+numbers =  [randint(1, 100) for i in range(j)]
+def filter_odd_num(i):
+    if(i % 2) != 0:
         return True
     else:
         return False
 odd_numbers = filter(filter_odd_num, numbers)
 print("Изначальный список: ", list(numbers))
 print("Отфильтрованный список: ", list(odd_numbers))
+
+def factorial_recursion(n):  
+   if n == 1:  
+       return n  
+   else:  
+       return n*factorial_recursion(n-1)
+# Вызов функции
+num = numbers[j-1]
+print("The factorial of ",num," is ",factorial_recursion(num))
