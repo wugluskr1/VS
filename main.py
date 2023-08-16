469471f9435e1a69089902457e371012d6e0d30e
wugluskr1 <54072591+wugluskr1@users.noreply.github.com>
2023-08-16 15:10:52 +0300

    Доработаны функции

--- main.py
+++ main.py
@@ -3,22 +3,43 @@
 #3. Напишите функцию, которая при каждом следующем своем вызове выводит на консоль следующий элемент заданного списка.
 from random import randint
 
-j = int(input("Введите число символов в массиве: "))
+j = int(input("Введите число символов в списке: "))
 numbers =  [randint(1, 100) for i in range(j)]
-def filter_odd_num(i):
-    if(i % 2) != 0:
-        return True
-    else:
-        return False
+
+def filter_odd_num(n):
+    if (n < 2):
+        return (n % 2 != 0)
+    return (filter_odd_num(n - 2))
 odd_numbers = filter(filter_odd_num, numbers)
 print("Изначальный список: ", list(numbers))
 print("Отфильтрованный список: ", list(odd_numbers))
+
+def length(lst):
+    if not lst:
+        return 0
+    return 1 + length(lst[1:])
+print("Длина списка равна: ", length(numbers))
+print(length(numbers))
 
 def factorial_recursion(n):  
    if n == 1:  
        return n  
    else:  
        return n*factorial_recursion(n-1)
-# Вызов функции
 num = numbers[j-1]
 print("The factorial of ",num," is ",factorial_recursion(num))
+
+def numbers_generate(lst):
+    lst = iter(lst)
+    return print(next(lst, 'STOP'))
+numbers_generate(numbers)
+
+def numbers_generate(lst):
+    lst = iter(lst)
+    try:
+        while True:
+            next_val = next(lst)
+            print("Значение:", next_val)
+    except StopIteration:
+        print("перебор завершен")
+numbers_generate(numbers)
